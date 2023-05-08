45d5d943e30e9ca738b1711b5db7c1afd0f334bc
Hermhades <36191927+Hermhades@users.noreply.github.com>
2023-05-08 16:03:02 +0800

    feat: simplify color config
    do not distinguish different types of style/colorStyle in weread any more, use the default style.

--- weread.py
+++ weread.py
@@ -128,26 +128,26 @@ def get_quote(content):
 
 def get_callout(content, style, colorStyle, reviewId):
     # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
-    emoji = "🌟"
-    if style == 0:
-        emoji = "💡"
-    elif style == 1:
-        emoji = "⭐"
+    emoji = "👣"
+#     if style == 0:
+#         emoji = "💡"
+#     elif style == 1:
+#         emoji = "⭐"
     # 如果reviewId不是空说明是笔记
     if reviewId != None:
         emoji = "✍️"
     color = "default"
     # 根据划线颜色设置文字的颜色
-    if colorStyle == 1:
-        color = "red"
-    elif colorStyle == 2:
-        color = "purple"
-    elif colorStyle == 3:
-        color = "blue"
-    elif colorStyle == 4:
-        color = "green"
-    elif colorStyle == 5:
-        color = "yellow"
+#     if colorStyle == 1:
+#         color = "red"
+#     elif colorStyle == 2:
+#         color = "purple"
+#     elif colorStyle == 3:
+#         color = "blue"
+#     elif colorStyle == 4:
+#         color = "green"
+#     elif colorStyle == 5:
+#         color = "yellow"
     return {
         "type": "callout",
         "callout": {
